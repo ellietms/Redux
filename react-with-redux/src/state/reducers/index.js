623a21b8881973d7c29b21f71686aca14ab5b7a3
ellie <ellie.tms1371@gmail.com>
2021-09-24 14:12:10 +0100

    Fix: fix error

--- react-with-redux/src/state/reducers/index.js
+++ react-with-redux/src/state/reducers/index.js
@@ -4,8 +4,7 @@ import {combineReducers} from "redux";
 import accountReducer from "./accountReducer";
 
 //  function that returns an object with a key which has the value from the reducer function
-const reducers = combineReducers({
+export const reducers = combineReducers({
     account: accountReducer
 })
 
-export default reducers;
--- react-with-redux/src/state/store.js
+++ react-with-redux/src/state/store.js
@@ -1,13 +1,9 @@
 //  our store
-
 import {createStore } from "redux";
-import reducers from "./reducers/index";
+import {reducers} from "./reducers/index";
 // reducers ,  default state
-
 //  create our store with reducers function we get from index.js and an empty {} which is our state
 // const store = createStore(reducers, {})
-const store = ( ) => {
-    return (createStore(reducers, {}))
-}
+export const store =  createStore(reducers, {})
 
-export default store;
+
