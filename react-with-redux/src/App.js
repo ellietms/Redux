eb580e17c580253c0c9719ad909b2e2fd47f2993
ellie <ellie.tms1371@gmail.com>
2021-09-24 21:26:01 +0100

    updated: app.js

--- react-with-redux/src/App.js
+++ react-with-redux/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import {useSelector , useDispatch} from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreaters } from "./state/index";
+import { depositeMoney } from './state/action-creators';
 
 function App() {
+  //  returning state inside of our store.js
+  const account = useSelector((state) => state.account);
+  //  dispatch the action creators
+  const dispatch = useDispatch();
+  const {depositeMoney , withdrawMoney} = bindActionCreators(actionCreaters, dispatch)
   return (
     <div className="App">
+      {account}
+     <button onClick={() => depositeMoney(1000)}> Deposit </button>  
+     <button onClick={() => withdrawMoney(500)}> Withdraw</button>
      <p> World!</p>
     </div>
   );
