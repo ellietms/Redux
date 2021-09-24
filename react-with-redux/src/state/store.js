6948f849f90da0af21513e151dee6bbbc615468d
ellie <ellie.tms1371@gmail.com>
2021-09-24 21:26:25 +0100

    updated: store.js

--- react-with-redux/src/state/store.js
+++ react-with-redux/src/state/store.js
@@ -1,9 +1,10 @@
 //  our store
-import {createStore } from "redux";
+import {applyMiddleware, createStore } from "redux";
 import {reducers} from "./reducers/index";
+import thunk from "redux-thunk";
 // reducers ,  default state
 //  create our store with reducers function we get from index.js and an empty {} which is our state
 // const store = createStore(reducers, {})
-export const store =  createStore(reducers, {})
+export const store =  createStore(reducers, {}, applyMiddleware(thunk))
 
 
